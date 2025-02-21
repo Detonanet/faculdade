b1e0827a826b9f266d7212d2aebbe925c248a30b
RAFAEL RODRIGO CORREA <86055574+Detonanet@users.noreply.github.com>
2025-02-21 08:41:32 -0300

    Add files via upload

---
+++ for_006.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ // Mostrando os numeros de 10 em 10 Incremento
+int main() {
+    for (int i = 0; i <= 500; i += 10) {
+        if (i != 0)
+        {
+            printf("%d\n", i);
+        }
+        
+    }
+   
+    return 0;
+}
