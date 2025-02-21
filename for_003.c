7fcd1201556b1045e4edb1233708461c19681223
RAFAEL RODRIGO CORREA <86055574+Detonanet@users.noreply.github.com>
2025-02-21 08:39:00 -0300

    Create for_003.c

---
+++ for_003.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    for(int i = 1; i <= 20; i++){
+
+        if(i == 10){
+            printf("Já contamos até a metade: %d\n",i);
+        } else
+        {
+            printf("Estamos em: %d\n",i);
+        }
+    
+        
+    }
+}
